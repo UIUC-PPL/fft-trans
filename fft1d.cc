6910e11e0ed9854731bba2d76cbd5e246edae9d9
Anshu Arya <arya3@illinois.edu>
2011-10-25 01:01:02 -0500

    Moved out a couple functions

--- fft1d.cc
+++ fft1d.cc
@@ -147,38 +147,13 @@ struct fft : public CBase_fft {
       }
   }
 
-  void initValidation() {
-    memcpy(in, out, sizeof(fftw_complex) * n);
+  void initValidation();
 
-    validating = true;
-    fftw_destroy_plan(p1);
-    int length[] = {N};
-    p1 = fftw_plan_many_dft(1, length, N/numChares, out, length, 1, N,
-                            out, length, 1, N, FFTW_BACKWARD, FFTW_ESTIMATE);
-
-    contribute(CkCallback(CkIndex_Main::startFFT(), mainProxy));
-  }
-
-  void calcResidual() {
-    double infNorm = 0.0;
-
-    srand48(thisIndex);
-    for(int i = 0; i < n; i++) {
-      out[i][0] = out[i][0]/(N*N) - drand48();
-      out[i][1] = out[i][1]/(N*N) - drand48();
-
-      double mag = sqrt(pow(out[i][0], 2) + pow(out[i][1], 2));
-      if(mag > infNorm) infNorm = mag;
-    }
-
-    double r = infNorm / (std::numeric_limits<double>::epsilon() * log((double)N * N));
-
-    CkCallback cb(CkReductionTarget(Main, printResidual), mainProxy);
-    contribute(sizeof(double), &r, CkReduction::max_double, cb);
-  }
+  void calcResidual();
 
   fft(CkMigrateMessage* m) {}
   ~fft() {}
 };
 
+#include "verify.h"
 #include "fft1d.def.h"
