2d66d03bcf7cdc27547c6daf9295aa9e7a09ce29
Anshu Arya <arya3@illinois.edu>
2012-10-24 23:41:43 -0500

    SUBMISSION: remove all unnecessary CkPrintfs

--- main.cc
+++ main.cc
@@ -26,16 +26,12 @@ struct Main : public CBase_Main {
     N = atol(m->argv[1]);
     delete m;
 
-    if (N % CkNumPes() != 0)
-      CkAbort("CkNumPes() not a factor of N\n");
-
     startBenchmark();
   }
 
   void init(int sign, CkCallback cb) {
     TopoManager tmgr; // get dimensions for software routing
     int dims[4] = {tmgr.getDimNZ(), tmgr.getDimNY(), tmgr.getDimNX(), tmgr.getDimNT()};
-    CkPrintf("Running on NX %d NY %d NZ %d NT %d\n", dims[0], dims[1], dims[2], dims[3]);
 
     fftProxy = CProxy_fft::ckNew(N, dataProxy, sign, cb);
     streamer = streamer_t::ckNew(BUFSIZE, 4, dims, fftProxy);
