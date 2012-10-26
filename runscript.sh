44782f14a8af74fcb05e799bf7b6ce4b507c9de1
Anshu Arya <arya3@illinois.edu>
2012-10-26 20:33:19 +0000

    Update runscript to use N and NUMBUF as inputs

--- runscript.sh
+++ runscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NODES=$1
-PROCS=$2
+N=$2
 NUMBUF=$3
 
-qsub -t 10 -n ${NODES} -A CharmRTS --mode vn --env BG_MAPPING=ZYXT ./fft1d${PROCS} ${NUMBUF}
+qsub -t 10 -n ${NODES} -A CharmRTS --mode vn --env BG_MAPPING=ZYXT ./fft1d ${N} ${NUMBUF}
